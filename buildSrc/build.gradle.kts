a82c3623b23391beddac4da7628ccfdabbc1691f
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2026-02-18 03:42:26 +0300

    bumping Kotlin version;

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,6 @@ plugins {
 
 dependencies {
     implementation("com.anatawa12.forge:ForgeGradle:1.2-1.1.+")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.10")
     implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:2.1.0")
 }
